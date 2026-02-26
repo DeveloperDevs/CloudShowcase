5880357957bae5a9f7fe51c8fd6c583ed470d96f
Devin <devinleemd@gmail.com>
2026-02-26 06:04:46 -0500

    Change CodeDeploy Scripts

--- ansible/roles/legacy/files/download_artifacts.sh
+++ ansible/roles/legacy/files/download_artifacts.sh
@@ -2,13 +2,15 @@
 set -e
 
 echo "Creating CodeDeploy scripts directory..."
-
 sudo mkdir -p /opt/codedeploy-scripts
 
-echo "Creating mock install script..."
-echo 'echo Hello from CodeDeploy script' | sudo tee /opt/codedeploy-scripts/install.sh
+echo "Downloading install.sh from S3 bucket base-template-devin..."
 
+# Download from S3
+sudo aws s3 cp s3://base-template-devin/install.sh /opt/codedeploy-scripts/install.sh
+
+echo "Setting execute permissions..."
 sudo chmod +x /opt/codedeploy-scripts/install.sh
 
-echo "Artifacts prepared."
+echo "Artifacts downloaded and prepared."
 echo "AMI Provisioning Complete"
