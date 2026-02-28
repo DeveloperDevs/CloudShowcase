4dbd39e6be8df6f30ca1bd33f05a9309b3b61b5c
Devin <devinleemd@gmail.com>
2026-02-28 13:11:26 -0500

    Improve Ansible Playbook

--- ansible/roles/legacy/files/download_artifacts.sh
+++ ansible/roles/legacy/files/download_artifacts.sh
@@ -4,13 +4,13 @@ set -e
 echo "Creating CodeDeploy scripts directory..."
 sudo mkdir -p /opt/codedeploy-scripts
 
-echo "Downloading install.sh from S3 bucket base-template-devin..."
+echo "Downloading product1.zip from S3 bucket base-template-devin..."
 
 # Download from S3
-sudo aws s3 cp s3://base-template-devin/install.sh /opt/codedeploy-scripts/install.sh
+sudo aws s3 cp s3://base-template-devin/product1.zip /opt/codedeploy-scripts/product1.zip
 
 echo "Setting execute permissions..."
-sudo chmod +x /opt/codedeploy-scripts/install.sh
+sudo chmod +x /opt/codedeploy-scripts/product1.zip
 
 echo "Artifacts downloaded and prepared."
 echo "AMI Provisioning Complete"
--- ansible/roles/legacy/files/install_dependencies.sh
+++ ansible/roles/legacy/files/install_dependencies.sh
@@ -4,7 +4,11 @@ set -e
 echo "Updating yum"
 sudo yum update -y
 
-echo "Installing AWS CLI and Ruby..."
-sudo yum install -y awscli ruby
+echo "Installing AWS CLI and SSM Agent..."
+sudo yum install -y awscli amazon-ssm-agent
+
+echo "Enabling and starting SSM Agent..."
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 
 echo "Dependencies installed."
